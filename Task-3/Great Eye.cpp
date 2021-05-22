ff2ae705763e4ef76250c7915d70d120aa9801f7
AcV134 <79892617+AcV134@users.noreply.github.com>
2021-05-22 23:49:40 +0530

    Update Great Eye.cpp

--- Task-3/Great Eye.cpp
+++ Task-3/Great Eye.cpp
@@ -1,14 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-#define fast ios::sync_with_stdio(0);cin.tie(0);
-
 
 int main(){
-    fast;
     int t;
     cin>>t;
-    while(t--){
+    for(;t>0;t--){
         int n;
         cin>>n;
         string name;
